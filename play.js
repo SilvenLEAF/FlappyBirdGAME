d457025dbf9bbad1e40e86f93f32490980350ec4
SilvenLEAF <SilvenLEAF@gmail.com>
2020-12-30 09:55:24 +0530

    SET cheatsheet

--- play.js
+++ play.js
@@ -2,5 +2,13 @@
 const mySet1 = new Set(`2323577`);
 const mySet2 = new Set([3,5,6,6,3,4,2,6,5,]);
 
+/*
+  add(x) -------chainable
+  delete(x) -------returns boolean
+  clear() -------returns undefined
+  size -------length
+  has(x) -------boolean
+*/
+
 console.log(mySet1);
 console.log(mySet2);
