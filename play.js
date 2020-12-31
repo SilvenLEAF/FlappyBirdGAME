b6ef7594ce72f8ae579e6c38851f733833b38757
SilvenLEAF <SilvenLEAF@gmail.com>
2020-12-31 09:00:30 +0530

    Playing with Map object

--- play.js
+++ play.js
@@ -12,3 +12,19 @@ const mySet2 = new Set([3,5,6,6,3,4,2,6,5,]);
 
 console.log(mySet1);
 console.log(mySet2);
+
+
+
+// Playing with MAP
+const myMap = new Map();
+
+console.log(myMap.set(4, 'number'));
+console.log(myMap.set(false, 'boolean'));
+console.log(myMap.has(4));
+console.log(myMap.has('boolean'));
+console.log(myMap.get(false));
+console.log(myMap.keys(['a', 'b', 'c', 'd', 'e']));
+console.log(myMap.size);
+console.log(myMap);
+console.log(myMap.clear());
+console.log(myMap);
