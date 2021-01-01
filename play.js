3e30023fe6b2b7f13ec80f2536fe638706e1394c
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-01 07:29:40 +0530

    mastered JavaScript SET, MAP and DATE objects

--- play.js
+++ play.js
@@ -1,45 +1,45 @@
-// // Playing with SET
-// const mySet1 = new Set(`2323577`);
-// const mySet2 = new Set([3,5,6,6,3,4,2,6,5,]);
+// Playing with SET
+const mySet1 = new Set(`2323577`);
+const mySet2 = new Set([3,5,6,6,3,4,2,6,5,]);
 
-// /*
-//   add(x) -------chainable
-//   delete(x) -------returns boolean
-//   clear() -------returns undefined
-//   size -------length
-//   has(x) -------boolean
-// */
+/*
+  add(x) -------chainable
+  delete(x) -------returns boolean
+  clear() -------returns undefined
+  size -------length
+  has(x) -------boolean
+*/
 
-// console.log('-----------------SET-----------------');
-// console.log(mySet1);
-// console.log(mySet2);
+console.log('-----------------SET-----------------');
+console.log(mySet1);
+console.log(mySet2);
 
 
 
-// // Playing with MAP
-// const myMap = new Map();
+// Playing with MAP
+const myMap = new Map();
 
-// /*
-//   set ------returns the map
-//   get ------returns the value
-//   has ------returns boolean
-//   size ------length
-//   clear ------returns undefined
-// */
+/*
+  set ------returns the map
+  get ------returns the value
+  has ------returns boolean
+  size ------length
+  clear ------returns undefined
+*/
 
-// console.log('-----------------MAP-----------------');
-// console.log(myMap.set(4, 'number'));
-// console.log(myMap.set(false, 'boolean'));
+console.log('-----------------MAP-----------------');
+console.log(myMap.set(4, 'number'));
+console.log(myMap.set(false, 'boolean'));
 
-// console.log(myMap.has(4));
-// console.log(myMap.has('boolean'));
+console.log(myMap.has(4));
+console.log(myMap.has('boolean'));
 
-// console.log(myMap.get(false));
-// console.log(myMap.size);
+console.log(myMap.get(false));
+console.log(myMap.size);
 
-// console.log(myMap);
-// console.log(myMap.clear());
-// console.log(myMap);
+console.log(myMap);
+console.log(myMap.clear());
+console.log(myMap);
 
 
 
