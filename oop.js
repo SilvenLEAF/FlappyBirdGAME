7b72d636dbd2f96f25f6fd09387e8154f93a9b7d
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-02 08:36:17 +0530

    Protyping on ES5 OOP

--- oop.js
+++ oop.js
@@ -7,5 +7,12 @@ function Person (name, age, isFemale){
 
 
 
+// PROTOTYPE
+Person.prototype.say = function(){
+  console.log(`Hi there! I'm ${ this.name }, a ${ this.age } years old ${ this.isFemale ? 'girl' : 'boy' }. Nice to meet you!`)
+}
+
+
 const alex = new Person('Alex', 27, false);
 console.log(alex);
+alex.say();
