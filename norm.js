3adbf62db795d6d2d29d74bb2b4edfe9abc47ec4
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-06 06:04:42 +0530

    difference between for of and for in

---
+++ norm.js
@@ -0,0 +1,20 @@
+// for in VS for of
+
+const myArr = [0,1,2,3,4,5,6,7,8,9]
+myArr.prop = 'A';
+
+console.log(myArr);
+
+/*
+for of -------includes prop
+for in -------excludes prop
+*/
+
+for(num of myArr){
+  console.log('num', num);
+}
+for(item in myArr){
+  console.log('item', item);
+}
+
+
