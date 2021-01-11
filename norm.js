2564652953d554f698aee83ccab88a33e20911da
Manash Sarma <SilvenLEAF@gmail.com>
2021-01-11 10:41:16 +0530

    Learning forEach in details

--- norm.js
+++ norm.js
@@ -18,3 +18,14 @@ for(item in myArr){
 }
 
 
+// Playing with forEach( )
+/*
+forEach( ) takes a callback function as an argument 
+and that function takes the item, index and the parent array 
+as its 1st, 2nd and 3rd arguments respectively 
+*/
+
+const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+myArr.forEach((item, index, arr)=>{
+  console.log({ item, index, arr })
+});
